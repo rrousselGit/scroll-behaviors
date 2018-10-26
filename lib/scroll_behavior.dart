f15f8dfbcf605089d75bf2b1e7dffdfcc1652107
Remi Rousselet <darky12s@gmail.com>
2018-10-26 15:52:24 +0200

    rename

--- lib/scroll_behavior.dart
+++ lib/scroll_behavior.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 
-class MyBehavior extends ScrollBehavior {
+/// A [ScrollBehavior] that does nothing.
+/// It can be used to remove the glowing effect of [MaterialApp]
+class NoScrollBehavior extends ScrollBehavior {
   @override
   Widget buildViewportChrome(
       BuildContext context, Widget child, AxisDirection axisDirection) {
