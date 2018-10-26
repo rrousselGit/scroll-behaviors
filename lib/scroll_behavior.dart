d8291c5ba7c3f503cf5f5f0f47242791552148fb
Remi Rousselet <darky12s@gmail.com>
2018-10-26 15:49:52 +0200

    init

---
+++ lib/scroll_behavior.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
